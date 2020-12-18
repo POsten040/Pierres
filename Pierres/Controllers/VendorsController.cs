56bb559274fc375fcb19f4e48ddcb26fd87c6151
Patrick Osten <Patrickosten040@gmail.com>
2020-12-18 10:55:15 -0800

    add views for new and show vendor

--- Pierres/Controllers/VendorsController.cs
+++ Pierres/Controllers/VendorsController.cs
@@ -14,5 +14,26 @@ namespace Pierres.Controllers
       List<Vendor> allVendors = Vendor.GetAll();
       return View(allVendors);
     }
+    [HttpPost("/vendors")]
+    public ActionResult Create(string name, string description)
+    {
+      Vendor newVendor = new Vendor(name, description);
+      return RedirectToAction("Index");
+    }
+    [HttpGet("/vendors/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+    [HttpGet("vendors/{Id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.Find(id);
+      List<Order> vendorOrders = selectedVendor.Orders;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
   }
 }
